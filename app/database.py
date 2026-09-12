e85df8b5900b17cb7f172abc083ced8d6aef9f91
Shefali Mandal <shefalimandal110@gmail.com>
2026-09-12 00:49:01 -0500

    Fix database configuration for CI

--- app/database.py
+++ app/database.py
@@ -1,25 +1,40 @@
 import os
 
 from dotenv import load_dotenv
-from sqlalchemy import create_engine
+from sqlalchemy import URL, create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 
 load_dotenv()
 
 
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
+def build_database_url():
+    db_host = os.getenv("DB_HOST")
+    db_port = os.getenv("DB_PORT")
+    db_name = os.getenv("DB_NAME")
+    db_user = os.getenv("DB_USER")
+    db_password = os.getenv("DB_PASSWORD")
+
+    if all([
+        db_host,
+        db_port,
+        db_name,
+        db_user,
+        db_password
+    ]):
+        return URL.create(
+            "postgresql+psycopg",
+            username=db_user,
+            password=db_password,
+            host=db_host,
+            port=int(db_port),
+            database=db_name
+        )
+
+    return "sqlite:///experiments.db"
 
 
-DATABASE_URL = (
-    f"postgresql+psycopg://"
-    f"{DB_USER}:{DB_PASSWORD}"
-    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-)
+DATABASE_URL = build_database_url()
 
 
 engine = create_engine(
